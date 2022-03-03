eba60a1ee22252bc4352b5af53ac1f0478b87035
unknown <bleachevil@gmail.com>
2022-03-02 19:46:25 -0500

    Add PyBank

---
+++ PyBank/PyBank.py
@@ -0,0 +1,43 @@
+import csv
+import pprint
+import numpy as np
+from numpy import average
+
+rows = []
+element_in_lists = False
+element_in_list = False
+
+file = open('budget_data.csv')
+csvreader = csv.reader(file)
+header = next(csvreader)
+#print(header)
+for row in csvreader:
+    rows.append(row)
+
+# split the list:
+res1, res2 = map(list, zip(*rows))
+
+# change string to int:
+for i in range(0, len(res2)):
+    res2[i] = int(res2[i])
+
+
+
+
+print("Financial Analysis")
+print("----------------------")
+print(f'Total Months: {len(res1)}')
+print(f'Total: {sum(res2)}')
+print(f'Average Change: ${np.round(average(res2),2)}')
+# search the greatest Increase from the original list:
+Increase = max(res2)
+for Great in rows:
+    if str(Increase) in Great:
+        print(f'Greatest Increase in Profits: {Great}')
+        
+
+# search the greatest Decrease from the original list:
+Decrease = min(res2)
+for low in rows:
+    if str(Decrease) in low:
+        print(f'Greatest Decrease in Profits: {low}')
